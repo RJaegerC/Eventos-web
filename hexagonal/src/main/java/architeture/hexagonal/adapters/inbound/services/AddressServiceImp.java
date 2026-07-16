2f23aec9f0068d8c96eee07eb4e571ff8c05881b
Jaeger <Lnkcyrillo@gmail.com>
2026-07-16 10:14:57 -0300

    Update AddressServiceImp.java

--- hexagonal/src/main/java/architeture/hexagonal/adapters/inbound/services/AddressServiceImp.java
+++ hexagonal/src/main/java/architeture/hexagonal/adapters/inbound/services/AddressServiceImp.java
@@ -1,32 +1,32 @@
 package architeture.hexagonal.adapters.inbound.services;
 
-import architeture.hexagonal.application.services.AddressService;
 import architeture.hexagonal.application.usecases.AddressUseCases;
 import architeture.hexagonal.models.adress.Address;
 import architeture.hexagonal.models.adress.AddressRepository;
 import architeture.hexagonal.models.event.Event;
 import architeture.hexagonal.models.event.EventRequestDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class AddressServiceImp implements AddressUseCases {
 
-    private final AddressUseCases useCase;
-
-    public AddressServiceImp(AddressRepository addressRepository) {
-        this.useCase = new AddressService(addressRepository);
-    }
+    private final AddressRepository addressRepository;
 
     @Override
     public void createAddress(EventRequestDTO data, Event event) {
-        useCase.createAddress(data, event);
+        Address address = new Address();
+        address.setCity(data.city());
+        address.setUf(data.state());
+        address.setEventId(event.getId());
     }
 
     @Override
     public Optional<Address> findByEventId(UUID eventId) {
-        return useCase.findByEventId(eventId);
+        return addressRepository.findByEventId(eventId);
     }
 }
