7bd0b630b3c44f8a9039f80135ebfe7555876006
Jaeger <Lnkcyrillo@gmail.com>
2026-07-16 10:01:54 -0300

    Update CouponServiceImp.java

--- hexagonal/src/main/java/architeture/hexagonal/adapters/inbound/services/CouponServiceImp.java
+++ hexagonal/src/main/java/architeture/hexagonal/adapters/inbound/services/CouponServiceImp.java
@@ -1,11 +1,12 @@
 package architeture.hexagonal.adapters.inbound.services;
 
-import architeture.hexagonal.application.services.CouponService;
 import architeture.hexagonal.application.usecases.CouponUseCases;
 import architeture.hexagonal.models.coupon.Coupon;
 import architeture.hexagonal.models.coupon.CouponRepository;
 import architeture.hexagonal.models.coupon.CouponRequestDTO;
+import architeture.hexagonal.models.event.Event;
 import architeture.hexagonal.models.event.EventRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -13,27 +14,28 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class CouponServiceImp implements CouponUseCases {
 
-    private final CouponUseCases useCase;
-
-    public CouponServiceImp(
-            CouponRepository couponRepository,
-            EventRepository eventRepository
-    ) {
-        this.useCase = new CouponService(
-                couponRepository,
-                eventRepository
-        );
-    }
+    private final CouponRepository couponRepository;
+    private final EventRepository eventRepository;
 
     @Override
     public Coupon addCouponToEvent(UUID eventId, CouponRequestDTO couponData) {
-        return useCase.addCouponToEvent(eventId, couponData);
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new IllegalArgumentException("Event not found"));
+
+        Coupon coupon = new Coupon();
+        coupon.setCode(couponData.code());
+        coupon.setDiscount(couponData.discount());
+        coupon.setValid(new Date(couponData.valid()));
+        coupon.setEventId(event.getId());
+
+        return coupon;
     }
 
     @Override
     public List<Coupon> consultCoupons(UUID eventId, Date currentDate) {
-        return useCase.consultCoupons(eventId, currentDate);
+        return couponRepository.findByEventIdAndValidAfter(eventId, currentDate);
     }
 }
